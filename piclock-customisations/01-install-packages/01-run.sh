668f512a636affa736901eade8ef4e4731764f51
Simon Hyde <simon@icedrop.net>
2024-04-19 09:40:29 +0100

    Fixup ntpdate bugfix
    Incorrect variable substition meant this was fixing the local package
    on the build host instead of the built image

--- piclock-customisations/01-install-packages/01-run.sh
+++ piclock-customisations/01-install-packages/01-run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -e
 
 #Fixup bug in if-up scripts
-sed -i -e 's/^service=/export service=/' "$(ROOTFS_DIR)/etc/network/if-up.d/ntpsec-ntpdate"
+sed -i -e 's/^service=/export service=/' "${ROOTFS_DIR}/etc/network/if-up.d/ntpsec-ntpdate"
