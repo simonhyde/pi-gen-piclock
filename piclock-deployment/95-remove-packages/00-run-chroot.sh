5952e4058b89388eaca271c413ce1e6dee6bf153
Simon Hyde <simon@icedrop.net>
2024-06-11 17:10:56 +0100

    Tweaks for arm64 build

--- piclock-deployment/95-remove-packages/00-run-chroot.sh
+++ piclock-deployment/95-remove-packages/00-run-chroot.sh
@@ -3,5 +3,5 @@
 #Unnecessary packages which were seen to be installed
 export DEBIAN_FRONTEND=noninteractive
 export SUDO_FORCE_REMOVE=yes
-apt-get purge -y -f less vim-tiny vim-common nano gcc gcc-12 ghostscript fakeroot manpages paxctld ppp shared-mime-info tasksel triggerhappy xz-utils zstd curl dos2unix userconf-pi htop make dpkg-dev traceroute systemd-timesyncd
+apt-get purge -y -f less vim-tiny vim-common nano gcc gcc-12 ghostscript fakeroot manpages ppp shared-mime-info tasksel triggerhappy xz-utils zstd curl dos2unix userconf-pi htop make dpkg-dev traceroute systemd-timesyncd
 apt-get autoremove --purge -y -f
